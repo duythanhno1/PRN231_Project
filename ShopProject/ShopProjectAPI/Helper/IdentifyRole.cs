5d487bbf7bbe4f2b49a3865b2001bc803be2c624
datle202 <ledat8875@gmail.com>
2024-02-22 21:02:45 +0700

    up
    upp

--- ShopProject/ShopProjectAPI/Helper/IdentifyRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShopProjectAPI.Helper
-{
-    public class IdentifyRole
-    {
-        public const string AdminRole = "Admin";
-        public const string RoleRequired = "admin";
-    }
-}
--- ShopProject/ShopProjectAPI/Helper/TokenReponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using BussinessObject.Models;
-namespace ShopProjectAPI.Helper
-{
-    public class TokenReponse
-    {
-        public Account Account { get; set; }
-        public string AccessToken { get; set; }
-    }
-}
